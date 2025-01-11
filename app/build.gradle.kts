19808458148900f2f9d16a66abfda033bd047ef6
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-01-11 17:15:10 +0700

    Codelab Pengujian DAO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,4 +84,8 @@ dependencies {
     //special testing
     testImplementation(libs.androidx.core.testing) // InstantTaskExecutorRule
     testImplementation(libs.kotlinx.coroutines.test) //TestCoroutineDispatcher
+
+    //special instrumentation testing
+    androidTestImplementation(libs.androidx.core.testing) // InstantTaskExecutorRule
+    androidTestImplementation(libs.kotlinx.coroutines.test) //TestDispatcher
 }
